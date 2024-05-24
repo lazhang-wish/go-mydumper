018797685ac909184719fd7e50e80f817e89c1dc
lance zhang <lazhang@wish.com>
2024-05-24 17:21:59 +0800

    revert sql dumper to enable 0000-00-00 of date

--- common/dumper.go
+++ common/dumper.go
@@ -137,18 +137,7 @@ func dumpTable(log *xlog.Log, conn *Connection, args *config.Config, database st
 
 				case "BINARY":
 					byteCol := col.([]byte)
-					values = append(values, "0x"+fmt.Sprintf(`"%x"`, EscapeBytes(byteCol)))
-				case "DATE":
-					byteCol := col.([]byte)
-					zeroDate := []byte{48, 48, 48, 48, 45, 48, 48, 45, 48, 48}
-
-					if bytes.Equal(byteCol, zeroDate) {
-						values = append(values, "NULL")
-						rowsize += 4
-					} else {
-						values = append(values, fmt.Sprintf("%s", EscapeBytes(byteCol)))
-						rowsize += len(byteCol) + 2
-					}
+					values = append(values, "0x"+fmt.Sprintf("%x", byteCol))
 				default:
 					// TODO:  check/test the following types
 					// "BINARY" "BIT" "BLOB" "DATE" "DATETIME" "DOUBLE" "ENUM" "GEOMETRY"
